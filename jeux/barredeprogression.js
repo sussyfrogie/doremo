907eef3087c60dacf0f94f62f9a933c7acd42fc3
arcdragons <147136724+arcdragons@users.noreply.github.com>
2023-11-29 14:50:57 +0100

    js

--- jeux/barredeprogression.js
+++ jeux/barredeprogression.js
@@ -1,17 +1,19 @@
-var score = 0;
+const score = 0;
 const bare = document.createElement("div");
 console.log(1111);
-if (score=0) {
+if (score==0) {
     for (let i=0;i==4;i++){
         const point = document.bare.appendChild(document.createElement("div"))
         point.classList.add("negatif");
+        console.log(10)
     }
     for (let i=0;i==1;i++){
         const point1 = document.bare.appendChild(document.createElement("div"))
         point1.classList.add("positif");
+        console.log(5)
     }
 };
-if (score=1) {
+if (score==1) {
     for (let i=0;i==3;i++){
         const point = document.bare.appendChild(document.createElement("div"))
         point.classList.add("negatif");
@@ -21,7 +23,7 @@ if (score=1) {
         point1.classList.add("positif");
     }
 };
-if (score=2) {
+if (score==2) {
     for (let i=0;i==2;i++){
         const point = document.bare.appendChild(document.createElement("div"))
         point.classList.add("negatif");
@@ -31,7 +33,7 @@ if (score=2) {
         point1.classList.add("positif");
     }
 };
-if (score=3) {
+if (score==3) {
     for (let i=0;i==1;i++){
         const point = document.bare.appendChild(document.createElement("div"))
         point.classList.add("negatif");
@@ -41,7 +43,7 @@ if (score=3) {
         point1.classList.add("positif");
     }
 };
-if (score=4) {
+if (score==4) {
     for (let i=0;i==5;i++){
         const point1 = document.bare.appendChild(document.createElement("div"))
         point1.classList.add("positif");
