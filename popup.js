0f0a8ae1b364d3eabb65f52bbb5773bc53e8ebcd
sussyfrogie <anastasia.leonita@edu.gobelins.fr>
2023-11-30 12:34:38 +0100

    popup

---
+++ popup.js
@@ -0,0 +1,7 @@
+document.getElementById('openPopup').addEventListener('click', function () {
+    document.getElementById('popupContainer').style.display = 'flex';
+  });
+  
+  document.getElementById('closePopup').addEventListener('click', function () {
+    document.getElementById('popupContainer').style.display = 'none';
+  });
