2af2851801b478a0277371772a13c2751e154be4
sussyfrogie <anastasia.leonita@edu.gobelins.fr>
2024-12-28 21:22:09 +0100

    confetti

--- main.js
+++ main.js
@@ -383,6 +383,7 @@ if (window.location.href.includes("jeux.html")) {
             const music = new Audio("sons/victoire.mp3");
             music.play(Audio);
             music.volume = 0.5;
+            //countdownInstance.stop();
         } 
          //cobaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
         level.style.height = `${conteur * 25}%`;
@@ -461,3 +462,48 @@ if (window.location.href.includes("jeux.html")) {
         }, 6000);
     }
 
+//coba
+const Confettiful = function(el){
+    this.el = el;
+    this.containerEl = null;
+    this.confettiFrequency = 3;
+    this.confettiColors = ['#FF8E01','#FF2F93','#01A24C','#FBCA04'];
+    this.confettiAnimations = ['slow','medium','fast'];
+    this._setupElements();
+    this._renderConfetti();
+  };
+  
+  Confettiful.prototype._setupElements = function(){
+    const containerEl = document.createElement('div');
+    const elPosition = this.el.style.position;
+    if(elPosition!=='relative' || elPosition!=='absolute'){
+      this.el.style.position='relative';
+    }
+    containerEl.classList.add('confetti-container');
+    this.el.appendChild(containerEl);
+    this.containerEl = containerEl;
+  };
+  
+  Confettiful.prototype._renderConfetti = function(){
+    this.confettiInterval = setInterval(()=>{
+      const confettiEl = document.createElement('div');
+      const confettiSize = (Math.floor(Math.random()*3)+7)+'px';
+      const confettiBackground = this.confettiColors[Math.floor(Math.random()*this.confettiColors.length)];
+      const confettiLeft = (Math.floor(Math.random()*this.el.offsetWidth))+'px';
+      const confettiAnimation = this.confettiAnimations[Math.floor(Math.random()*this.confettiAnimations.length)];
+      confettiEl.classList.add('confetti','confetti--animation-'+confettiAnimation);
+      confettiEl.style.left = confettiLeft;
+      confettiEl.style.width = confettiSize;
+      confettiEl.style.height = confettiSize;
+      confettiEl.style.backgroundColor = confettiBackground;
+      confettiEl.removeTimeout = setTimeout(function(){
+        confettiEl.parentNode.removeChild(confettiEl);
+      },3000);
+      this.containerEl.appendChild(confettiEl);
+    },25);
+  };
+  
+  window.confettiful = new Confettiful(document.querySelector('.js-container.container'));
+  
+//
+makeContainerFullScreen('.js-container.container');
